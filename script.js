a8d0ff8b1201276895760c4706ad917f644d5ceb
lavsora <ya.karpich@gmail.com>
2022-05-27 13:33:52 +0300

    tasks of the second lesson

---
+++ script.js
@@ -0,0 +1,20 @@
+let title, screens, screenPrice, rollback, fullPrice, adaptive;
+
+title = "Training project";
+screens = "Simple, Complex, Interactive";
+screenPrice = 50;
+rollback = 24;
+fullPrice = 256;
+adaptive = false;
+
+console.log(typeof title, typeof fullPrice, typeof adaptive);
+
+console.log(screens.length);
+
+console.log("Стоимость верстки экранов " + screenPrice + " долларов");
+console.log("Стоимость разработки сайта" + " " + fullPrice + " " + "долларов")
+
+console.log(screens.toLocaleLowerCase());
+console.log(screens.split(', '));
+
+console.log(fullPrice * (rollback/100));
