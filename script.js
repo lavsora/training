d422b6237b65d349dba498ff130a72513a20b825
lavsora <ya.karpich@gmail.com>
2022-05-30 15:26:18 +0300

    switch/case construction test

--- script.js
+++ script.js
@@ -1,18 +1,46 @@
-const title = "Training project";
-let screens = "Simple, Complex, Interactive";
-const screenPrice = 50;
+const title = prompt('Как называется ваш проект?');
+let screens = prompt('Какие типы экранов нужно разработать?', 'Простые, Сложные, Интерактивные');
+const screenPrice = +prompt('Сколько будет стоить данная работа?');
+let adaptive = confirm('Нужен ли адаптив на сайте?');
+
+const service1 = prompt('Какой дополнительный тип услуги нужен?');
+const servicePrice1 = +prompt('Сколько это будет стоить?');
+const service2 = prompt('Какой дополнительный тип услуги нужен?');
+const servicePrice2 = +prompt('Сколько это будет стоить?');
+
 const rollback = 24;
-const fullPrice = 256;
-const adaptive = false;
+const fullPrice = screenPrice + servicePrice1 + servicePrice2;
 
-console.log(typeof title, typeof fullPrice, typeof adaptive);
+const servicePercentPrice = Math.ceil(fullPrice - rollback);
 
-console.log(screens.length);
+console.log('Чистая прибыль ' + servicePercentPrice + ' рублей');
 
-console.log("Стоимость верстки экранов " + screenPrice + " долларов");
-console.log("Стоимость разработки сайта " + fullPrice + " долларов");
+switch(true) {
+    case fullPrice >= 30000:
+        console.log('Даем скидку в 10%');
+        break;
+    case fullPrice >= 15000 && fullPrice < 30000:
+        console.log('Даем скидку в 5%');
+        break;
+    case fullPrice < 15000 && fullPrice >= 0:
+        console.log('Скидка не предусмотрена');
+        break;
+    case fullPrice < 0:
+        console.log('Что то пошло не так');
+        break;
+    default:
+        console.log('NaN');
+        break;
+}
 
-console.log(screens.toLocaleLowerCase());
-console.log(screens.split(', '));
+// console.log(typeof title, typeof fullPrice, typeof adaptive);
 
-console.log("Процент отката посреднику за работу " + fullPrice * (rollback/100) + "%");
+// console.log(screens.length);
+
+// console.log("Стоимость верстки экранов " + screenPrice + " долларов");
+// console.log("Стоимость разработки сайта " + fullPrice + " долларов");
+
+// console.log(screens.toLocaleLowerCase());
+// console.log(screens.split(', '));
+
+// console.log("Процент отката посреднику за работу " + fullPrice * (rollback/100) + "%");
